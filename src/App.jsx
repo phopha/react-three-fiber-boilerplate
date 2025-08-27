cf98842dcdb3bc74a96fcde654ff52591211287b
phopha <148693028+phopha@users.noreply.github.com>
2025-08-27 10:52:51 +0700

    23. Environment

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import { Stats, OrbitControls, Circle } from '@react-three/drei'
+import { Stats, OrbitControls, Environment } from '@react-three/drei'
 import { Canvas, useLoader } from '@react-three/fiber'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader'
 
 export default function App() {
-  const gltf = useLoader(GLTFLoader, '/models/monkey.glb')
+  const gltf = useLoader(GLTFLoader, './models/monkey.glb')
 
   return (
-    <Canvas camera={{ position: [-0.5, 1, 2] }} shadows>
+    <Canvas camera={{ position: [-0.5, 1, 2] }} >
+      <Environment files="./img/venice_sunset_1k.hdr" background/>
       <directionalLight
         position={[3.3, 1.0, 4.4]}
         castShadow
@@ -15,11 +16,7 @@ export default function App() {
       <primitive
         object={gltf.scene}
         position={[0, 1, 0]}
-        children-0-castShadow
       />
-      <Circle args={[10]} rotation-x={-Math.PI / 2} receiveShadow>
-        <meshStandardMaterial />
-      </Circle>
       <OrbitControls target={[0, 1, 0]} />
       <axesHelper args={[5]} />
       <Stats />

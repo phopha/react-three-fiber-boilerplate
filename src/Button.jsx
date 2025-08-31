a5ceb22465ea1f80d8d7a1b35c119b0880ec5bf1
phopha <148693028+phopha@users.noreply.github.com>
2025-09-01 00:48:47 +0700

    33. Lerp : Part 2

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,6 +1,29 @@
+import { useState, useRef} from 'react'
+import { useFrame } from '@react-three/fiber'
+import { MathUtils, Color} from 'three';
+
+const black = new Color('black');
 export default function Button(props){
+  const ref = useRef();
+  const [hovered, setHovered] = useState();
+  const [selected, setSelected] = useState(false);
+  const colorTo = new Color(0x00ff00);
+
+  useFrame(() => {
+    ref.current.rotation.x = hovered ? MathUtils.lerp(ref.current.rotation.x, -Math.PI * 2, 0.025) : MathUtils.lerp(ref.current.rotation.x, 0, 0.025);
+
+    ref.current.position.z = selected ? MathUtils.lerp(ref.current.position.z, 0, 0.025) : MathUtils.lerp(ref.current.position.z, -3, 0.025);
+
+    ref.current.material.color.lerp(selected ? colorTo : black, 0.025)
+  });
   return (
-    <mesh {...props}>
+    <mesh 
+      {...props}
+      ref={ref} 
+      onPointerDown={() => setSelected(!selected)}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
       <icosahedronGeometry/>
       <meshPhysicalMaterial
         roughness={0}

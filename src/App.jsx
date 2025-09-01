d47ee8bd2e8d1abd2d97cd6f30893ee0e83c2daf
phopha <148693028+phopha@users.noreply.github.com>
2025-09-01 15:00:02 +0700

    34. Custom Hooks : Part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,15 @@
-import { Canvas, useFrame, useThree} from '@react-three/fiber'
-import { Center, Environment } from '@react-three/drei'
-import Button from './Button.jsx'
-import { Vector3 } from 'three'
+import { Canvas } from '@react-three/fiber'
+import Box from './Box'
+import { Stats, OrbitControls } from '@react-three/drei'
 
-
-function Rig(){
-  const {camera, mouse} = useThree();
-  const vec = new Vector3();
-  useFrame(() => {
-    vec.set(mouse.x, mouse.y, camera.position.z);
-    camera.position.lerp(vec, 0.01);
-    camera.lookAt(0, 0, 0);
-    
-  })
-}
 export default function App() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 8] }}>
-      <Environment preset="forest" background/>
-      <Center>
-
-      {[...Array(5).keys()].map((x) =>
-          [...Array(3).keys()].map((y) => (
-            <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
-          ))
-        )}
-      </Center>
-      
-      <Rig/>
+    <Canvas camera={{ position: [1, 2, 3] }}>
+      <Box position={[0, 0.5, 0]} />
+      <OrbitControls />
+      <axesHelper args={[5]} />
+      <gridHelper />
+      <Stats />
     </Canvas>
   )
 }
---
+++ src/Box.jsx
@@ -0,0 +1,23 @@
+import { useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+import useKeyboard from './useKeyboard'
+
+export default function Box(props) {
+  const ref = useRef()
+  const keyMap = useKeyboard()
+
+  useFrame((_, delta) => {
+    keyMap['KeyA'] && (ref.current.position.x -= 1 * delta)
+    keyMap['KeyD'] && (ref.current.position.x += 1 * delta)
+    keyMap['KeyW'] && (ref.current.position.z -= 1 * delta)
+    keyMap['KeyS'] && (ref.current.position.z += 1 * delta)
+    console.log(ref)
+  })
+
+  return (
+    <mesh ref={ref} {...props}>
+      <boxGeometry />
+      <meshBasicMaterial color={0x00ff00} wireframe />
+    </mesh>
+  )
+}
